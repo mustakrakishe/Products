798e2c3d68b94c98a6d9e0706a50cfa589ebde21
mustakrakishe <mustakrkish@gmail.com>
2022-04-23 20:42:10 +0300

    Create Locale entity && repository

--- src/Entity/Country.php
+++ src/Entity/Country.php
@@ -4,6 +4,8 @@ namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CountryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CountryRepository::class)]
@@ -18,6 +20,14 @@ class Country
     #[ORM\Column(type: 'string', length: 255)]
     private $name;
 
+    #[ORM\OneToMany(mappedBy: 'country', targetEntity: Locale::class, orphanRemoval: true)]
+    private $locales;
+
+    public function __construct()
+    {
+        $this->locales = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
@@ -31,6 +41,36 @@ class Country
     public function setName(string $name): self
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Locale>
+     */
+    public function getLocales(): Collection
+    {
+        return $this->locales;
+    }
+
+    public function addLocale(Locale $locale): self
+    {
+        if (!$this->locales->contains($locale)) {
+            $this->locales[] = $locale;
+            $locale->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLocale(Locale $locale): self
+    {
+        if ($this->locales->removeElement($locale)) {
+            // set the owning side to null (unless already changed)
+            if ($locale->getCountry() === $this) {
+                $locale->setCountry(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Locale.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\LocaleRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: LocaleRepository::class)]
+#[ApiResource]
+class Locale
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $name;
+
+    #[ORM\Column(type: 'string', length: 2)]
+    private $iso;
+
+    #[ORM\ManyToOne(targetEntity: Country::class, inversedBy: 'locales')]
+    #[ORM\JoinColumn(nullable: false)]
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getIso(): ?string
+    {
+        return $this->iso;
+    }
+
+    public function setIso(string $iso): self
+    {
+        $this->iso = $iso;
+
+        return $this;
+    }
+
+    public function getCountry(): ?Country
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?Country $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
